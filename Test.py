f9ca0416cfbd8da94bcfdb9fbeb018d47e0bb5b6
Shankar_9012 <shankardas9012@gmail.com>
2022-10-19 22:24:11 +0545

    Initial commit

---
+++ Test.py
@@ -0,0 +1,55 @@
+print("Hello world")
+age=18
+#print(name)
+new_age=age + 2
+print(new_age)
+A=input("What is your name ?")
+print("you name Is :", A)
+(Age)=input("Enter your age")
+print(int(Age) + 2)
+first=input("Enter first number")
+second=input("Enter second number")
+sum = int(first) + int (second)
+print("the sum is :", sum)
+name="shankar"
+print(name.upper())
+print(name.find("s"))
+print('s' in name)
+print(5%2)
+print(5 ** 2)
+i= 5
+i +=2
+print(3==3)
+print(3!=3)
+print(2>3 or 3<2)
+if age >=18:
+ print("You are and adult")
+ print("You can vote")
+elif age<18 and age >3:
+ print("You are kiddo")
+else:
+ print("You are bsdk")
+ i=1
+ while i<=5:
+  print(i *  ' *')
+  i+=1
+for i range(5):
+ print(i)
+ marks=[25,26,27]
+ for score in marks:
+  if score==27:
+   break;
+   print(marks)
+ print(marks[-1])
+ print(marks[0:2])
+ marks.append(99)
+ print(marks.index(26))
+ print(99 in marks)
+ print(len(marks))
+ from math import sqrt
+ print(sqrt(16))
+def print_sum(first,second):
+ print(first+ second)
+ print_sum(1,2)
+
+
